e7ad213baa795bb8edea6d8801fb732a15dc02c8
c vw <dl1ycf@darc.de>
2022-01-17 17:24:17 +0100

    Replaced default font by FL_SCREEN

--- src/widgets/FTextView.cxx
+++ src/widgets/FTextView.cxx
@@ -68,7 +68,7 @@ FTextBase::FTextBase(int x, int y, int w, int h, const char *l)
 {
 	oldw = oldh = olds = -1;
 	oldf = (Fl_Font)-1;
-	textfont(FL_HELVETICA);
+	textfont(FL_SCREEN);
 	textsize(FL_NORMAL_SIZE);
 	textcolor(FL_FOREGROUND_COLOR);
 
@@ -477,11 +477,11 @@ int FTextBase::reset_wrap_col(void)
 
 void FTextBase::reset_styles(int set)
 {
-	set_style(NATTR, FL_HELVETICA, FL_NORMAL_SIZE, FL_FOREGROUND_COLOR, set);
-	set_style(XMIT, FL_HELVETICA, FL_NORMAL_SIZE, FL_RED, set);
-	set_style(CTRL, FL_HELVETICA, FL_NORMAL_SIZE, FL_DARK_GREEN, set);
-	set_style(SKIP, FL_HELVETICA, FL_NORMAL_SIZE, FL_BLUE, set);
-	set_style(ALTR, FL_HELVETICA, FL_NORMAL_SIZE, FL_DARK_MAGENTA, set);
+	set_style(NATTR, FL_SCREEN, FL_NORMAL_SIZE, FL_FOREGROUND_COLOR, set);
+	set_style(XMIT, FL_SCREEN, FL_NORMAL_SIZE, FL_RED, set);
+	set_style(CTRL, FL_SCREEN, FL_NORMAL_SIZE, FL_DARK_GREEN, set);
+	set_style(SKIP, FL_SCREEN, FL_NORMAL_SIZE, FL_BLUE, set);
+	set_style(ALTR, FL_SCREEN, FL_NORMAL_SIZE, FL_DARK_MAGENTA, set);
 }
 
 // ----------------------------------------------------------------------------
